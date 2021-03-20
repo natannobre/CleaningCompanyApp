d3c71744d2b7a50b2545f9fbab81f3ae6b8b0eec
ruanfelipe7 <felipealmeida903@gmail.com>
2021-03-19 22:56:06 -0300

    Versao 1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyparser = require('body-parser');
+const app = express();
+const path = require('path');
+
+//Configuração do body-parser
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json())
+
+//Configuração do handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');
+
+//Public
+app.use(express.static(path.join(__dirname, "public")))
+
+const port = 8888;
+const host = "localhost"
+
+
+app.get("/", (req, res) => {
+    res.redirect("/login");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.listen(port, host, () => {
+    console.log("Servidor executando na porta " + port);
+})
+
+
